076e4a8fe65a247456db533a1be91bd181445972
PhanNgocCanh <109947955+PhanNgocCanh@users.noreply.github.com>
2023-03-14 21:20:05 +0700

    Create test.java

---
+++ src/general/static_key/test.java
@@ -0,0 +1,7 @@
+package general.static_key;
+
+public class MostAppearent {
+    public static void main(String[] args) {
+        System.out.println("pull a file from remote repository");
+    }
+}
