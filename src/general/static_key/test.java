272a0dbdde6e893184e3da8039d3524287c33ae4
PhanNgocCanh <109947955+PhanNgocCanh@users.noreply.github.com>
2023-03-14 21:21:00 +0700

    Delete test.java

--- src/general/static_key/test.java
+++
@@ -1,7 +0,0 @@
-package general.static_key;
-
-public class MostAppearent {
-    public static void main(String[] args) {
-        System.out.println("pull a file from remote repository");
-    }
-}
