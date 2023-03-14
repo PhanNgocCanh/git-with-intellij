57b8320a2eea06bbff580b57a7a8a76d30f0cb23
PhanNgocCanh <canhrapter@gmail.com>
2023-03-14 21:23:35 +0700

    commit file test

--- src/general/static_key/test.java
+++ src/general/static_key/test.java
@@ -4,5 +4,6 @@ package general.static_key;
 public class test {
     public static void main(String[] args) {
         System.out.print("pull a file from remote repository");
+        System.out.println("pull file success");
     }
 }
